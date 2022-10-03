16e35b363314524ef6846a5a7ae3a6c84c2d3876
Roberto <robertosilva.2@hotmail.com>
2022-10-02 23:36:42 -0300

    corrigindo footer

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const buttons= document.querySelectorAll('.controls button');
 
 buttons.forEach(button => {
   button.addEventListener('click', e => {
-    document.querySelector('.box').classList.add('rotate-scale-down-ver');
+    document.querySelector('.box').classList.add('.rotate-scale-down-over');
     setTimeout(() => {
-      document.querySelector('.box').classList.remove('rotate-scale-down-ver');
+      document.querySelector('.box').classList.remove('.rotate-scale-down-over');
     }, 650);
   })
 });
